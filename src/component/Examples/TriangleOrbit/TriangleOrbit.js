a5791d18ee7dc7496db9ad4fa6df75caab79e9f0
gandis <gandis0713@gmail.com>
2020-04-17 23:37:03 +0900

    implement camera orbit

--- src/component/Examples/TriangleOrbit/TriangleOrbit.js
+++ src/component/Examples/TriangleOrbit/TriangleOrbit.js
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { createShader, createShaderProgram } from '../../../webgl/shader/Shader'
 import { vertexShaderSource, fragmentShaderSource } from './ShaderSource'
-import Slider from '@material-ui/core/Slider';
-import Typography from '@material-ui/core/Typography';
+import {vec3, mat4} from 'gl-matrix'
 
 import { vertices, colors } from './resources'
-import {mat4} from './mat'
-import {vec3} from './vec'
+
+
+const camEye = vec3.create();
+camEye[0] = 0;
+camEye[1] = 0;
+camEye[2] = 100;
+const camUp = vec3.create();
+camUp[0] = 0;
+camUp[1] = 1;
+camUp[2] = 0;
+const camTar = vec3.create();
+const MCVC = mat4.create();
+const VCPC = mat4.create();
+const MCPC = mat4.create();
 
 function TriangleOrbit() {
 
@@ -24,11 +35,70 @@ function TriangleOrbit() {
   let translationMatrixUniformLocation;
 
   let prePosition = [0, 0];
+  
+  let width = 0;
+  let height = 0;
+  let halfWidth = 0;
+  let halfHeight = 0;
 
-  let mat = mat4.translation(0, 0, 0);
+  const onMounted = function() {
+
+    // initialize
+    glCanvas = document.getElementById("_glcanvas");
+    glCanvas.addEventListener("mousedown", mouseDownEvent , false);
+    glCanvas.addEventListener("mousemove", mouseMoveEvent , false);
+    glCanvas.addEventListener("mouseup", mouseUpEvent , false);
+    glContext = glCanvas.getContext("webgl2");
+
+    if(!glContext) {
+      alert("Unable to initialize WebGL.");
+      return;
+    }
+    
+    width = glContext.canvas.width;
+    height = glContext.canvas.height;
+    halfWidth = width / 2;
+    halfHeight = height / 2;
+
+    glContext.viewport(0, 0, width, height);
+    glContext.clearColor(0.0, 0.0, 0.0, 1.0);
+    glContext.clearDepth(1.0);
+    glContext.enable(glContext.DEPTH_TEST);
+    glContext.depthFunc(glContext.LEQUAL);
+    
+    glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
+
+    // create shader
+    const vertexShader = createShader(glContext, glContext.VERTEX_SHADER, vertexShaderSource);
+    const fragmentShader = createShader(glContext, glContext.FRAGMENT_SHADER, fragmentShaderSource);
+
+    shaderProgram = createShaderProgram(glContext, vertexShader, fragmentShader);
+
+    translationMatrixUniformLocation = glContext.getUniformLocation(shaderProgram, 'uTransformMatrix');
+
+    // initialize buffer
+    vertexBuffer = glContext.createBuffer();
+    glContext.bindBuffer(glContext.ARRAY_BUFFER, vertexBuffer);
+
+    glContext.bufferData(glContext.ARRAY_BUFFER, new Float32Array(vertices), glContext.STATIC_DRAW);
+
+    colorBuffer = glContext.createBuffer();
+    glContext.bindBuffer(glContext.ARRAY_BUFFER, colorBuffer);
+
+    glContext.bufferData(glContext.ARRAY_BUFFER, new Uint8Array(colors), glContext.STATIC_DRAW);
+
+    // init camera
+    mat4.lookAt(MCVC, camEye, camTar, camUp);
+    mat4.ortho(VCPC, -halfWidth, halfWidth, -halfHeight, halfHeight, 1000, -1000);
+    mat4.multiply(MCPC, VCPC, MCVC);
+
+    
+    drawScene();
+  }
   
   const drawScene = function() {
     if(!glContext) {
+      console.log(" glContext return ");
       return;
     }    
 
@@ -58,10 +128,8 @@ function TriangleOrbit() {
     )
     glContext.enableVertexAttribArray(colorID);
 
-    const projection = mat4.orthographic(-glContext.canvas.width / 2, glContext.canvas.width / 2, -glContext.canvas.height / 2, glContext.canvas.height / 2, -1000, 1000);
-
     glContext.useProgram(shaderProgram);
-    glContext.uniformMatrix4fv(translationMatrixUniformLocation, false, new Float32Array(mat4.multiply(projection, mat)));
+    glContext.uniformMatrix4fv(translationMatrixUniformLocation, false, MCPC);
 
     glContext.drawArrays(glContext.TRIANGLES, 0, vertices.length / 3);
   }
@@ -69,80 +137,61 @@ function TriangleOrbit() {
   const mouseMoveEvent = (event) => {
     if(isDragging === true) {
       
-      const diffX = event.offsetX - prePosition[0];
-      const diffY = event.offsetY - prePosition[1];
+      const diffX = halfWidth - event.offsetX - prePosition[0];
+      const diffY = event.offsetY - halfHeight - prePosition[1];
 
       const screenNormal = [0, 0, 1];
-      const dir = [-diffX, -diffY, 0];
-      let axis = vec3.cross(dir, screenNormal);
+      const dir = [diffX, diffY, 0];
+      const axis = vec3.create();
+      vec3.cross(axis, dir, screenNormal);
 
-      axis = vec3.normalize(axis[0], axis[1], axis[2]);
+      vec3.normalize(axis, axis);
+      
+      let dgreeX = vec3.dot(axis, [1, 0, 0]);
+      let dgreeY = vec3.dot(axis, [0, 1, 0]);
 
-      mat4.rotationA(0.02, axis[0], axis[1], axis[2], mat);
+      dgreeX = dgreeX * 3.141592 / 180.0;
+      dgreeY = dgreeY * 3.141592 / 180.0;
 
-      prePosition[0] = event.offsetX;
-      prePosition[1] = event.offsetY;
+      const camTarToEye = vec3.create();
+      vec3.subtract(camTarToEye, camEye, camTar);
+      vec3.normalize(camTarToEye, camTarToEye);
+      const camRight = vec3.create();
+      vec3.cross(camRight, camUp, camTarToEye);
+
+      const camPitch = mat4.create();
+      mat4.fromRotation(camPitch, dgreeX, camRight);
+      const camYaw = mat4.create();
+      mat4.fromRotation(camYaw, dgreeY, camUp);
+
+      vec3.transformMat4(camEye, camEye, camPitch);
+      vec3.transformMat4(camEye, camEye, camYaw);
+
+      vec3.subtract(camTarToEye, camEye, camTar);
+      vec3.cross(camUp, camTarToEye, camRight);
+      vec3.normalize(camUp, camUp);
+
+      mat4.lookAt(MCVC, camEye, camTar, camUp);
+      mat4.multiply(MCPC, VCPC, MCVC);
+
+      prePosition[0] = halfWidth - event.offsetX;
+      prePosition[1] = event.offsetY - halfHeight;
+      
+      drawScene();
     }
-    drawScene();
   }
 
   const mouseDownEvent = (event) => {
     isDragging = true;
-    prePosition[0] = event.offsetX;
-    prePosition[1] = event.offsetY;
+
+    prePosition[0] = halfWidth - event.offsetX;
+    prePosition[1] = event.offsetY - halfHeight;
+    
+    drawScene();
   }
 
   const mouseUpEvent = (event) => {
     isDragging = false;
-  }
-
-  const onMounted = function() {
-
-    // initialize
-    glCanvas = document.getElementById("_glcanvas");
-    glCanvas.addEventListener("mousedown", mouseDownEvent , false);
-    glCanvas.addEventListener("mousemove", mouseMoveEvent , false);
-    glCanvas.addEventListener("mouseup", mouseUpEvent , false);
-    glContext = glCanvas.getContext("webgl2");
-
-    if(!glContext) {
-      alert("Unable to initialize WebGL.");
-      return;
-    }
-
-    glContext.viewport(0, 0, glContext.canvas.width, glContext.canvas.height)
-    glContext.clearColor(0.0, 0.0, 0.0, 1.0);
-    glContext.clearDepth(1.0);
-    glContext.enable(glContext.DEPTH_TEST);
-    glContext.depthFunc(glContext.LEQUAL);
-    
-    glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
-
-    // create shader
-
-    const vertexShader = createShader(glContext, glContext.VERTEX_SHADER, vertexShaderSource);
-    const fragmentShader = createShader(glContext, glContext.FRAGMENT_SHADER, fragmentShaderSource);
-
-    shaderProgram = createShaderProgram(glContext, vertexShader, fragmentShader);
-
-    translationMatrixUniformLocation = glContext.getUniformLocation(shaderProgram, 'uTransformMatrix');
-
-    // draw scene
-
-
-    // initialize buffer
-    vertexBuffer = glContext.createBuffer();
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, vertexBuffer);
-
-    glContext.bufferData(glContext.ARRAY_BUFFER, new Float32Array(vertices), glContext.STATIC_DRAW);
-
-    colorBuffer = glContext.createBuffer();
-    glContext.bindBuffer(glContext.ARRAY_BUFFER, colorBuffer);
-
-    glContext.bufferData(glContext.ARRAY_BUFFER, new Uint8Array(colors), glContext.STATIC_DRAW);
-
-
-    drawScene();
   }
 
   useEffect(onMounted, [])
