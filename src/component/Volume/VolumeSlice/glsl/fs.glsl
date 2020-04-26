ed62b16b4e17d7165f233804e12afa15ce040d8c
gandis <gandis0713@gmail.com>
2020-04-26 14:36:04 +0900

    change thickness

--- src/component/Volume/VolumeSlice/glsl/fs.glsl
+++ src/component/Volume/VolumeSlice/glsl/fs.glsl
@@ -7,8 +7,70 @@ in vec2 fs_textCoord;
 out vec4 outColor;
 
 uniform highp sampler3D u_texture;
+uniform vec3 u_Dim;
+uniform vec3 u_Extent;
+uniform vec3 u_Bounds;
+uniform vec3 u_Spacing;
+uniform float u_camThickness;
+uniform float u_camNear;
+uniform float u_camFar;
+uniform float u_camTar;
+uniform float u_width;
+uniform float u_height;
+uniform float u_depth;
+
+vec4 getTextureValue(vec3 coord)
+{
+  return texture(u_texture, coord);
+}
 
 void main() {
-  vec4 color = texture(u_texture, vec3(fs_textCoord, 0.5));
-  outColor = vec4(color.r, color.r, color.r, color.a);
+
+  float stepX = 1.0 / u_width;
+  float stepY = 1.0 / u_height;
+  float stepZ = 1.0 / u_depth;
+  highp int countX = int(ceil(u_camThickness / u_Spacing[0]));
+  highp int countY = int(ceil(u_camThickness / u_Spacing[1]));
+  highp int countZ = int(ceil(u_camThickness / u_Spacing[2]));
+  float coordX = u_camNear / u_width;
+  float coordY = u_camNear / u_height;
+  float coordZ = u_camNear / u_depth;
+  vec4 sum = vec4(0.);
+  for(int i = 0; i < countZ; i++)
+  {
+    // vec4 color = getTextureValue(vec3(float(i) * stepX, fs_textCoord.y, fs_textCoord.x));
+    // vec4 color = getTextureValue(vec3(float(i) * stepX, fs_textCoord.x, fs_textCoord.y));
+    // vec4 color = getTextureValue(vec3(fs_textCoord.x, float(i) * stepY, fs_textCoord.y));
+    // vec4 color = getTextureValue(vec3(fs_textCoord.y, float(i) * stepY, fs_textCoord.x));
+    vec4 color = getTextureValue(vec3(fs_textCoord.x, fs_textCoord.y, float(i) * stepZ));
+    // vec4 color = getTextureValue(vec3(fs_textCoord.y, fs_textCoord.x, float(i) * stepZ));
+    sum += vec4(color.r, color.r, color.r, 0.0);
+  }
+  sum /= float(countZ);
+  outColor = vec4(sum.rgb, 1.0);
 }
+
+// void main() {
+
+//   float stepX = 1.0 / u_Dim.x;
+//   float stepY = 1.0 / u_Dim.y;
+//   float stepZ = 1.0 / u_Dim.z;
+//   vec4 sum = vec4(0.);
+//   int start = 50;
+//   int end = 60;
+//   // highp int countZ = int(u_Dim.z);
+//   int count = end - start;
+//   for(int i = start; i < end; i++)
+//   {
+//     // vec4 color = getTextureValue(vec3(float(i) * stepX, fs_textCoord.y, fs_textCoord.x));
+//     // vec4 color = getTextureValue(vec3(float(i) * stepX, fs_textCoord.x, fs_textCoord.y));
+//     // vec4 color = getTextureValue(vec3(fs_textCoord.x, float(i) * stepY, fs_textCoord.y));
+//     // vec4 color = getTextureValue(vec3(fs_textCoord.y, float(i) * stepY, fs_textCoord.x));
+//     vec4 color = getTextureValue(vec3(fs_textCoord.x, fs_textCoord.y, float(i) * stepZ));
+//     // vec4 color = getTextureValue(vec3(fs_textCoord.y, fs_textCoord.x, float(i) * stepZ));
+//     sum += vec4(color.r, color.r, color.r, 0.0);
+//   }
+//   sum /= float(count);
+//   outColor = vec4(sum.rgb, 1.0);
+// }
+
--- src/component/Volume/VolumeSlice/index.js
+++ src/component/Volume/VolumeSlice/index.js
@@ -5,15 +5,24 @@ import vertexShaderSource from './glsl/vs.glsl'
 import fragmentShaderSource from './glsl/fs.glsl'
 import {vec2, vec3, mat4} from 'gl-matrix'
 
+
+import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
 const camEye = vec3.create();
 camEye[0] = 0;
 camEye[1] = 0;
-camEye[2] = 1000;
+camEye[2] = 0;
 const camUp = vec3.create();
 camUp[0] = 0;
 camUp[1] = 1;
 camUp[2] = 0;
 const camTar = vec3.create();
+let camNear = 0.5;
+let camFar = -0.5;
 const MCVC = mat4.create();
 const VCPC = mat4.create();
 const MCPC = mat4.create();
@@ -34,22 +43,72 @@ let vbo_textCoordBuffer;
 let vbo_volumeBuffer;
 let vao;
 let u_MCPC;
+let u_Dim;
+let u_Extent;
+let u_Bounds;
+let u_Spacing;
+let u_camThickness;
+let u_camTar;
+let u_camNear;
+let u_camFar;
+let u_width;
+let u_height;
+let u_depth;
+
+let volume;
+
+const AxisType = {
+  axial: 0,
+  saggital: 1,
+  coronal: 2
+}
 
 function VolumeSlice() {
 
-  const [volume, setVolume] = useState();
+  const [axisType, setAxisType] = useState(AxisType.axial);
+  const [thickness, setThickness] = useState(1);
   
   const onMounted = function() {
-    console.log("on Mounted.");    
-
-
-
+    console.log("on Mounted."); 
     initView();
+  }
+
+  const onAxisChanged = function(event) {
+    console.log("axis : ", event.target.value);
+    setAxisType(event.target.value);
+  }
+
+  const onMouseWheel = function(event) {
+    const delta = event.deltaY > 0 ? 1 : -1;
+    if(axisType === AxisType.axial) {
+      camTar[0] = 0;
+      camTar[1] = 0;
+      camTar[2] += delta;
+
+      const halfThickness = thickness / 2;
+      camNear = camTar[2] + halfThickness;
+      camFar = camTar[2] - halfThickness;
+    }
+    console.log("camTar : ", camTar);
+    render();
+  }
+
+  const onThicknessChanged = function(event, value) {
+    console.log("thickness : ", value);
+    setThickness(value);
+
+    if(axisType === AxisType.axial) {
+      const halfThickness = value / 2;
+      camNear = camTar[2] + halfThickness;
+      camFar = camTar[2] - halfThickness;
+    }
+    render();
   }
 
   const initView = function() {
     
     const glCanvas = document.getElementById("glcanvas");
+    glCanvas.addEventListener('wheel', onMouseWheel, false); 
     gl = glCanvas.getContext("webgl2");
     if(!gl) {
       console.log("Failed to get gl context for webgl2.");
@@ -81,8 +140,22 @@ function VolumeSlice() {
 
     shaderProgram = createShaderProgram(gl, vertexShader, fragmentShader);
     u_MCPC = gl.getUniformLocation(shaderProgram, 'u_MCPC');
+    u_Dim = gl.getUniformLocation(shaderProgram, 'u_Dim');
+    u_Extent = gl.getUniformLocation(shaderProgram, 'u_Extent');
+    u_Bounds = gl.getUniformLocation(shaderProgram, 'u_Bounds');
+    u_Spacing = gl.getUniformLocation(shaderProgram, 'u_Spacing');
+    u_camThickness = gl.getUniformLocation(shaderProgram, 'u_camThickness');
+    u_camNear = gl.getUniformLocation(shaderProgram, 'u_camNear');
+    u_camFar = gl.getUniformLocation(shaderProgram, 'u_camFar');
+    u_camTar = gl.getUniformLocation(shaderProgram, 'u_camTar');
+    u_width = gl.getUniformLocation(shaderProgram, 'u_width');
+    u_height = gl.getUniformLocation(shaderProgram, 'u_height');
+    u_depth = gl.getUniformLocation(shaderProgram, 'u_depth');
+    
+    setBuffer();
+  }
 
-
+  const setBuffer = function() {
     xmlVtiReader(`/assets/volumes/dicom.vti`).then((imageData) => {
 
       vao = gl.createVertexArray();
@@ -112,10 +185,10 @@ function VolumeSlice() {
         gl.FLOAT,
         imageData.floatArray);
 
-      setVolume(imageData);
+      volume = imageData;
       
-      const imageWidth = imageData.bounds[1] - imageData.bounds[0];
-      const imageHeight = imageData.bounds[3] - imageData.bounds[2];
+      const imageWidth = (imageData.bounds[1] - imageData.bounds[0]) * 2;
+      const imageHeight = (imageData.bounds[3] - imageData.bounds[2]) * 2;
 
       vertices = [
         -imageWidth,  imageHeight,
@@ -154,8 +227,6 @@ function VolumeSlice() {
 
       render();
     });
-
-
   }
 
   const render = function() {
@@ -165,7 +236,20 @@ function VolumeSlice() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     
     gl.useProgram(shaderProgram);
+    mat4.lookAt(MCVC, camEye, camTar, camUp);
+    mat4.multiply(MCPC, VCPC, MCVC);
     gl.uniformMatrix4fv(u_MCPC, false, MCPC);
+    gl.uniform3fv(u_Dim, volume.dimension);
+    gl.uniform3fv(u_Extent, volume.extent);
+    gl.uniform3fv(u_Bounds, volume.bounds);
+    gl.uniform3fv(u_Spacing, volume.spacing);
+    gl.uniform1f(u_camThickness, thickness);
+    gl.uniform1f(u_camNear, camNear);
+    gl.uniform1f(u_camFar, camFar);
+    gl.uniform1f(u_camTar, camTar);
+    gl.uniform1f(u_width, volume.bounds[1] - volume.bounds[0]);
+    gl.uniform1f(u_height, volume.bounds[3] - volume.bounds[2]);
+    gl.uniform1f(u_depth, volume.bounds[5] - volume.bounds[4]);
     gl.bindVertexArray(vao);
 
     gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
@@ -175,7 +259,32 @@ function VolumeSlice() {
 
   return(
     <div>
-      <canvas id="glcanvas" width="800" height ="600"/>
+      <Divider />
+      <Grid container spacing={2}>
+        <Grid item>
+          <Typography variant="h6">Axis</Typography>
+        </Grid>
+        <Grid item>
+          <Select native value={axisType} onChange={onAxisChanged}>
+            <option value={AxisType.axial}>Axial</option>
+            <option value={AxisType.saggital}>Sagittal</option>
+            <option value={AxisType.coronal}>Coronal</option>
+          </Select>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Typography gutterBottom>Thickness</Typography>
+        </Grid>
+        <Grid item xs>
+          <Slider value={thickness} min={0} max={100} step={1} onChange={onThicknessChanged} />
+        </Grid>
+        <Grid item xs>
+          <Typography gutterBottom>{thickness}</Typography>
+        </Grid>
+      </Grid>
+      <Divider />
+      <canvas id="glcanvas" width="600" height ="500"/>
     </div>
   );
 }
